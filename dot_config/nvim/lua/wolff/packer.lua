f49ae6196b4517070995f87fb03e7bb937c8140e
Nick Wolff <nick@wolff.tech>
2024-01-23 17:29:57 -0500

    nvim: add tmux-navigator

--- dot_config/nvim/lua/wolff/packer.lua
+++ dot_config/nvim/lua/wolff/packer.lua
@@ -8,9 +8,9 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
 	use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.4',
+	'nvim-telescope/telescope.nvim', tag = '0.1.4',
 -- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
+	requires = { {'nvim-lua/plenary.nvim'} }
 }
 
 	use({
@@ -94,6 +94,7 @@ use('vim-airline/vim-airline-themes')
 use('ms-jpq/chadtree')
 use('ms-jpq/coq_nvim')
 use('kdheepak/lazygit.nvim')
-use('ThePrimeagen/vim-be-good')
+use('christoomey/vim-tmux-navigator')
 
 end)
+
