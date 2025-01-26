f4bb29aabee87ed45791dc57e79a901b6096df7e
Nick Wolff <nick@wolff.tech>
2025-01-25 19:58:49 -0500

    neovim: remove markdown plugins

--- dot_config/nvim/lua/wolff/plugins/markdownpreview.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	ft = { "markdown" },
-	build = function() vim.fn["mkdp#util#install"]() end,
-	vim.keymap.set("n", "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", { desc = "Open Markdown Preview" }),
-}
-
