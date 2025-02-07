8dca67b8721f632399b1e274f83fe9843fe41984
Nick Wolff <nick@wolff.tech>
2025-02-07 17:55:40 -0500

    neovim: fix keymap for telescope-fb

--- dot_config/nvim/lua/wolff/plugins/telescope-fb.lua
+++ dot_config/nvim/lua/wolff/plugins/telescope-fb.lua
@@ -2,5 +2,7 @@ return {
 	"nvim-telescope/telescope-file-browser.nvim",
 	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 
-	vim.keymap.set("n", "<space>fm", ":Telescope file_browser<CR>", { desc = "Open File Browser" })
+	config = function()
+		vim.keymap.set("n", "<space>fm", ":Telescope file_browser<CR>", { desc = "Open File Browser" })
+	end
 }
