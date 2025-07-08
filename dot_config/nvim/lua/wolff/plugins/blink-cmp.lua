7764676d181baecaf8cb62a8b2a57496c5a18847
Nick Wolff <nick@wolff.tech>
2025-07-08 00:11:52 -0400

    nvim: blink add spell

--- dot_config/nvim/lua/wolff/plugins/blink-cmp.lua
+++ dot_config/nvim/lua/wolff/plugins/blink-cmp.lua
@@ -11,6 +11,7 @@ return {
 			"rafamadriz/friendly-snippets",
 			"moyiz/blink-emoji.nvim",
 			"ray-x/cmp-sql",
+			'ribru17/blink-cmp-spell'
 		},
 
 		version = "1.*",
@@ -45,10 +46,34 @@ return {
 			signature = { enabled = true },
 
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				default = { "lsp", "path", "snippets", "buffer", "spell" },
 				providers = {
+					spell = {
+						name = 'Spell',
+						module = 'blink-cmp-spell',
+						opts = {
+							enable_in_context = function()
+								local curpos = vim.api.nvim_win_get_cursor(0)
+								local captures = vim.treesitter.get_captures_at_pos(
+									0,
+									curpos[1] - 1,
+									curpos[2] - 1
+								)
+								local in_spell_capture = false
+								for _, cap in ipairs(captures) do
+									if cap.capture == 'spell' then
+										in_spell_capture = true
+									elseif cap.capture == 'nospell' then
+										return false
+									end
+								end
+								return in_spell_capture
+							end,
+						},
+					},
 				},
 			},
+
 			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
 		opts_extend = { "sources.default" },
