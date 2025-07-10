209b498c149db598314202dc98c2df4441a7aa97
Nick Wolff <nick@wolff.tech>
2025-07-10 05:55:54 -0400

    nvim: update plugins reliant on themes to account for system appearance
    Using auto-dark-mode.lua, we can set a global variable to use in other
    plugins that will determine the systems current light/dark theme.
    An autocmd is used to then check for when auto-dark-mode updates that
    variable as it's preforming a constant check.
    
    This isn't perfect as this only checks the systems theme. If you are not
    using a terminal that also changes it's theme depending on the system
    appearance it will make nvims colors unusable. Should look into checking
    the terminal theme instead of the system theme, but this works for me
    since I use Ghostty or iTerm2 and have it set to change themes depending on the
    system appearance.

--- dot_config/nvim/lua/wolff/plugins/color.lua
+++ dot_config/nvim/lua/wolff/plugins/color.lua
@@ -1,36 +1,44 @@
 return {
-	{
 	'projekt0n/github-nvim-theme',
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-			require('github-theme').setup({
-				options = {
-					transparent = true,
-					dim_inactive = false,
-					styles = {
-						comments = 'italic',
-						functions = 'italic',
-						keywords = 'bold',
-						variables = 'NONE',
-						conditionals = 'NONE',
-						constants = 'NONE',
-						numbers = 'NONE',
-						operators = 'NONE',
-						strings = 'NONE',
-						types = 'bold',
+		require('github-theme').setup({
+			options = {
+				transparent = true,
+				dim_inactive = false,
+				styles = {
+					comments = 'italic',
+					functions = 'italic',
+					keywords = 'bold',
+					variables = 'NONE',
+					conditionals = 'NONE',
+					constants = 'NONE',
+					numbers = 'NONE',
+					operators = 'NONE',
+					strings = 'NONE',
+					types = 'bold',
+				},
+				darken = {
+					floats = true,
+					sidebars = {
+						enable = true,
+						lists = {},
 					},
-					darken = {
-						floats = true,
-						sidebars = {
-							enable = true,
-							lists = {},
-						},
-					},
-				}
+				},
+			}
 		})
+		local function apply_github_colors()
+			if vim.g.my_theme_mode == "light" then
+				vim.cmd("colorscheme github_light_default")
+			else
+				vim.cmd("colorscheme github_dark_default")
+			end
+		end
 
-		vim.cmd('colorscheme github_dark_default')
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "MyThemeChanged",
+			callback = apply_github_colors,
+		})
 	end,
-	}
 }
--- dot_config/nvim/lua/wolff/plugins/lualine.lua
+++ dot_config/nvim/lua/wolff/plugins/lualine.lua
@@ -1,192 +1,124 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "f-person/git-blame.nvim" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "f-person/git-blame.nvim",
+  },
   config = function()
-    -- Git blame plugin configuration
-    vim.g.gitblame_display_virtual_text = 0
-    vim.g.gitblame_message_template = "Git: <summary> - <sha> - <author>"
-    vim.g.gitblame_date_format = "%d/%m/%Y %H"
-    vim.g.gitblame_message_when_not_committed = "*Uncommitted Changes* - You"
+    vim.g.gitblame_display_virtual_text        = 0
+    vim.g.gitblame_message_template            = "Git: <summary> - <sha> - <author>"
+    vim.g.gitblame_date_format                 = "%d/%m/%Y %H"
+    vim.g.gitblame_message_when_not_committed  = "*Uncommitted Changes* - You"
 
     local dark_colors = {
-      grey    = "#757575",
-      gold    = "#afaf5f",
-      pink    = "#afaf5f",
-      blue    = "#5f87af",
-      orange  = "#d75f00",
-      fg      = "#FFFFFF",
-      mg      = "#4B4B4B",
-      bg      = "#101010",
-      xg      = "#212121",
-      middle  = "#bcbcbc",
+      grey   = "#757575"; gold   = "#afaf5f"; pink   = "#bC8cff";
+      blue   = "#5f87af"; orange = "#d75f00"; fg     = "#FFFFFF";
+      mg     = "#4B4B4B"; bg     = "#101010"; xg     = "#212121";
+      middle = "#bcbcbc";
     }
-
     local light_colors = {
-      grey    = "#757575",
-      gold    = "#afaf5f",
-      pink    = "#afaf5f",
-      blue    = "#5f87af",
-      orange  = "#d75f00",
-      fg      = "#000000",
-      mg      = "#9E9E9E",
-      bg      = "#FFFFFF",
-      xg      = "#F5F5F5",
-      middle  = "#4B4B4B",
+      grey   = "#757575"; gold   = "#afaf5f"; pink   = "#824fde";
+      blue   = "#5f87af"; orange = "#d75f00"; fg     = "#000000";
+      mg     = "#9E9E9E"; bg     = "#FFFFFF"; xg     = "#F5F5F5";
+      middle = "#4B4B4B";
     }
 
     local function get_lualine_theme()
-      if vim.o.background == "light" then
-        return {
-          normal = {
-            a = { bg = light_colors.grey, fg = light_colors.bg, gui = "bold" },
-            b = { bg = light_colors.mg, fg = light_colors.fg },
-            c = { bg = none, fg = light_colors.fg },
-            x = { bg = light_colors.xg, fg = light_colors.fg },
-          },
-          insert = {
-            a = { bg = light_colors.gold, fg = light_colors.bg, gui = "bold" },
-            b = { bg = light_colors.mg, fg = light_colors.fg },
-            c = { bg = none, fg = light_colors.fg },
-            x = { bg = light_colors.xg, fg = light_colors.fg },
-          },
-          visual = {
-            a = { bg = light_colors.pink, fg = light_colors.bg, gui = "bold" },
-            b = { bg = light_colors.mg, fg = light_colors.fg },
-            c = { bg = none, fg = light_colors.fg },
-            x = { bg = light_colors.xg, fg = light_colors.fg },
-          },
-          command = {
-            a = { bg = light_colors.grey, fg = light_colors.bg, gui = "bold" },
-            b = { bg = light_colors.mg, fg = light_colors.fg },
-            c = { bg = none, fg = light_colors.fg },
-            x = { bg = light_colors.xg, fg = light_colors.fg },
-          },
-          replace = {
-            a = { bg = light_colors.blue, fg = light_colors.bg, gui = "bold" },
-            b = { bg = light_colors.mg, fg = light_colors.fg },
-            c = { bg = none, fg = light_colors.fg },
-            x = { bg = light_colors.xg, fg = light_colors.fg },
-          },
-          inactive = {
-            a = { bg = light_colors.bg, fg = light_colors.middle, gui = "bold" },
-            b = { bg = light_colors.bg, fg = light_colors.middle },
-            c = { bg = light_colors.bg, fg = light_colors.middle },
-          },
-        }
-      else
-        return {
-          normal = {
-            a = { bg = dark_colors.grey, fg = dark_colors.bg, gui = "bold" },
-            b = { bg = dark_colors.mg, fg = dark_colors.fg },
-            c = { bg = none, fg = dark_colors.fg },
-            x = { bg = dark_colors.xg, fg = dark_colors.fg },
-          },
-          insert = {
-            a = { bg = dark_colors.gold, fg = dark_colors.bg, gui = "bold" },
-            b = { bg = dark_colors.mg, fg = dark_colors.fg },
-            c = { bg = none, fg = dark_colors.fg },
-            x = { bg = dark_colors.xg, fg = dark_colors.fg },
-          },
-          visual = {
-            a = { bg = dark_colors.pink, fg = dark_colors.bg, gui = "bold" },
-            b = { bg = dark_colors.mg, fg = dark_colors.fg },
-            c = { bg = none, fg = dark_colors.fg },
-            x = { bg = dark_colors.xg, fg = dark_colors.fg },
-          },
-          command = {
-            a = { bg = dark_colors.grey, fg = dark_colors.bg, gui = "bold" },
-            b = { bg = dark_colors.mg, fg = dark_colors.fg },
-            c = { bg = none, fg = dark_colors.fg },
-            x = { bg = dark_colors.xg, fg = dark_colors.fg },
-          },
-          replace = {
-            a = { bg = dark_colors.blue, fg = dark_colors.bg, gui = "bold" },
-            b = { bg = dark_colors.mg, fg = dark_colors.fg },
-            c = { bg = none, fg = dark_colors.fg },
-            x = { bg = dark_colors.xg, fg = dark_colors.fg },
-          },
-          inactive = {
-            a = { bg = dark_colors.bg, fg = dark_colors.middle, gui = "bold" },
-            b = { bg = dark_colors.bg, fg = dark_colors.middle },
-            c = { bg = dark_colors.bg, fg = dark_colors.middle },
-          },
-        }
-      end
+      local mode = vim.g.my_theme_mode or "dark"
+      local C = (mode == "light") and light_colors or dark_colors
+      local NONE = "NONE"
+
+      return {
+        normal = {
+          a = { bg = C.grey, fg = C.bg, gui = "bold" },
+          b = { bg = C.mg, fg = C.fg },
+          c = { bg = C.bg, fg = C.fg },
+          x = { bg = C.xg, fg = C.fg },
+        },
+        insert = {
+          a = { bg = C.gold, fg = C.bg, gui = "bold" },
+          b = { bg = C.mg, fg = C.fg },
+          c = { bg = C.bg, fg = C.fg },
+          x = { bg = C.xg, fg = C.fg },
+        },
+        visual = {
+          a = { bg = C.pink, fg = C.bg, gui = "bold" },
+          b = { bg = C.mg, fg = C.fg },
+          c = { bg = C.bg, fg = C.fg },
+          x = { bg = C.xg, fg = C.fg },
+        },
+        command = {
+          a = { bg = C.grey, fg = C.bg, gui = "bold" },
+          b = { bg = C.mg, fg = C.fg },
+          c = { bg = C.bg, fg = C.fg },
+          x = { bg = C.xg, fg = C.fg },
+        },
+        replace = {
+          a = { bg = C.blue, fg = C.bg, gui = "bold" },
+          b = { bg = C.mg, fg = C.fg },
+          c = { bg = C.bg, fg = C.fg },
+          x = { bg = C.xg, fg = C.fg },
+        },
+        inactive = {
+          a = { bg = C.bg, fg = C.middle, gui = "bold" },
+          b = { bg = C.bg, fg = C.middle },
+          c = { bg = C.bg, fg = C.middle },
+        },
+      }
     end
 
     local function setup_lualine()
-      local lualine = require("lualine")
-      local lazy_status = require("lazy.status")
-      local git_blame = require("gitblame")
-      local chosen_theme = get_lualine_theme()
+      local lualine      = require("lualine")
+      local lazy_status  = require("lazy.status")
+      local git_blame    = require("gitblame")
+      local theme        = get_lualine_theme()
 
       lualine.setup {
         options = {
-          icons_enabled = true,
-          theme = chosen_theme,
+          icons_enabled     = true,
+          theme             = theme,
           component_separators = { left = " ", right = " " },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
+          section_separators   = { left = "", right = "" },
           always_divide_middle = true,
-          globalstatus = false,
           refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
+            statusline = 1000, tabline = 1000, winbar = 1000,
           },
         },
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
           lualine_c = {
-            { "filename" },
-            {
-              git_blame.get_current_blame_text,
-              cond = git_blame.is_blame_text_available,
-            },
+            "filename",
+            { git_blame.get_current_blame_text,
+              cond = git_blame.is_blame_text_available },
           },
           lualine_x = {
-            {
-              lazy_status.updates,
-              cond = lazy_status.has_updates,
+            { lazy_status.updates,
+              cond  = lazy_status.has_updates,
               color = {
-                fg = vim.o.background == "light" and light_colors.orange or dark_colors.orange,
+                fg = (vim.g.my_theme_mode == "light")
+                     and light_colors.orange
+                     or dark_colors.orange,
               },
             },
-            "encoding",
-            "fileformat",
-            "filetype",
+            "encoding", "fileformat", "filetype",
           },
           lualine_y = { "progress" },
           lualine_z = { "location" },
         },
         inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
           lualine_c = { "filename" },
           lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
         },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
       }
-
-      vim.notify("lualine configured for '" .. vim.o.background .. "' background")
     end
 
     setup_lualine()
 
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      callback = function()
-        setup_lualine()
-      end,
+    vim.api.nvim_create_autocmd("User", {
+      pattern  = "MyThemeChanged",
+      callback = setup_lualine,
     })
   end,
 }
+
