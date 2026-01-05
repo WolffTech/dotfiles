afc5ba1a6a7076b478989e23cc046e9a975e4dd0
Nick Wolff <nick@wolff.tech>
2026-01-05 12:58:01 -0500

    nvim: update lazygit config
    Added hotkey for opening lazygit and autocmd to handle esc normally and
    use q to exit lazygit.

--- dot_config/nvim/lua/wolff/plugins/lazygit.lua
+++ dot_config/nvim/lua/wolff/plugins/lazygit.lua
@@ -1,6 +1,5 @@
 return {
-	'kdheepak/lazygit.nvim',
-
+	"kdheepak/lazygit.nvim",
 	cmd = {
 		"LazyGit",
 		"LazyGitConfig",
@@ -10,6 +9,26 @@ return {
 	},
 
 	dependencies = {
-		'nvim-lua/plenary.nvim',
+		"nvim-lua/plenary.nvim",
 	},
+
+	keys = {
+		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+	},
+
+	config = function()
+		vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { noremap = true, silent = true })
+		-- Keymaps for LazyGit buffer only
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "lazygit",
+			callback = function(args)
+				local buf = args.buf
+				local opts = { buffer = buf, noremap = true, silent = true }
+				-- close buffer with 'q'
+				vim.keymap.set("n", "q", "<cmd>close<CR>", opts)
+				-- Remap Esc to work properly in lazygit buffer
+				vim.keymap.set({ "i", "n", "t" }, "<Esc>", "<Esc>", opts)
+			end,
+		})
+	end,
 }
