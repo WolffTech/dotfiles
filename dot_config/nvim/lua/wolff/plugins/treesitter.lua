1b81ec941ab9f03b4eb17bd8a588d572d450a9db
Nick Wolff <nick@wolff.tech>
2026-01-05 10:05:40 -0500

    nvim: update treesitter to main branch
    The master branch of treesitter has been depricated in favor of the main
    branch which is a complete rewirte. Updating config for the new version
    of treesitter.
    
    Note that `tree-sitter-cli` and a C compiler are required for
    treesitter.

--- dot_config/nvim/lua/wolff/plugins/treesitter.lua
+++ dot_config/nvim/lua/wolff/plugins/treesitter.lua
@@ -1,48 +1,44 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	branch = 'main',
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
-			ensure_installed = {
-				"javascript", "typescript", "html", "css", "c", "lua", "rust",
-				"vim", "vimdoc", "query", "c_sharp", "arduino", "bash", "cmake",
-				"dockerfile", "go", "java", "json", "markdown", "python", "sql",
-				"terraform", "zig", "todotxt",
-			},
+		local ts = require("nvim-treesitter")
 
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			indent = {
-				enable = true
-			},
-
-			highlight = {
-				-- `false` will disable the whole extension
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = { "markdown" },
-			},
+		-- Setup with default install directory
+		ts.setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
 		})
 
-		local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		treesitter_parser_config.templ = {
-			install_info = {
-				url = "https://github.com/vrischmann/tree-sitter-templ.git",
-				files = {"src/parser.c", "src/scanner.c"},
-				branch = "master",
-			},
-		}
+		-- Pre-install these parsers
+		ts.install({
+			"javascript", "typescript", "html", "css", "c", "lua", "rust",
+			"vim", "vimdoc", "query", "c_sharp", "arduino", "bash", "cmake",
+			"dockerfile", "go", "java", "json", "markdown", "markdown_inline",
+			"python", "sql", "terraform", "zig", "todotxt", "templ",
+		})
 
+		-- Enable treesitter features for all filetypes
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local buf = args.buf
+				local ft = vim.bo[buf].filetype
+
+				-- Try to start treesitter highlighting
+				local ok = pcall(vim.treesitter.start, buf)
+
+				-- Auto-install parser if not available
+				if not ok and ft ~= "" then
+					ts.install({ ft })
+				end
+
+				-- Enable treesitter-based indentation
+				vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+
+		-- Register templ filetype
 		vim.treesitter.language.register("templ", "templ")
-	end
+	end,
 }
+
